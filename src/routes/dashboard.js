1c797e2d9acafe5fe8e9cbc99602f8b940e203d2
dickymahfudin <dickymahfudin13@gmail.com>
2023-02-07 23:08:57 +0700

    update

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -5,13 +5,27 @@ const { teacher, kriteria, user } = require('../models');
 
 router.get('/', async (req, res, next) => {
   let status = true;
-  const waspas = await teacher.findAll({
+  let waspas = await teacher.findAll({
     order: [['result', 'DESC']],
+    raw: true,
+    nest: true,
   });
   const kriterias = await kriteria.findAll();
   waspas.forEach(el => {
     if (!el.result) status = false;
   });
+  let rang = 0;
+  const temp = [];
+  for (let i = 0; i < waspas.length; i++) {
+    const el = waspas[i];
+    rang++;
+    if (i > 0) {
+      const old = temp[i - 1];
+      if (el.result == old.result) rang = old.rang;
+    }
+    temp.push({ ...el, rang });
+  }
+  waspas = temp;
   res.render('dashboard', { title: 'Dashboard', status, kriterias, waspas });
 });
 
